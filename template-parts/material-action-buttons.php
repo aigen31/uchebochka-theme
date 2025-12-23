819904a9b2aa6c53262c3d28b9e8e714e43ea44f
Dmitry <d.levchenko@elcomspb.ru>
2025-12-23 09:34:24 +0300

    Revert "Add edit material"
    This reverts commit 228e330d75806ece12dd113accd61dbb8c36dca8.

--- template-parts/material-action-buttons.php
+++ template-parts/material-action-buttons.php
@@ -8,9 +8,6 @@ if ($args['is_purchased'] || $args['is_free'] || current_user_can('administrator
     'metodic_pdfs' => 'Скачать PDF',
     'curses_file' => 'Скачать Курс',
   ];
-  if($args['metodic_material_once']){
-    $metodic_materials = $args['metodic_material_once'];
-  }
 
   foreach ($metodic_materials as $material_type => $button_text):
     $attachments = get_post_meta($post_id, $material_type);
@@ -19,15 +16,13 @@ if ($args['is_purchased'] || $args['is_free'] || current_user_can('administrator
       foreach ($attachments as $attachment_id):
         try {
           $attachment_id = intval($attachment_id);
-          if(!$args['metodic_material_once'])
           $download_link = $args['pda_services']->generate_custom_private_link($attachment_id, null, null);
           ?>
-          <div class='btn-buy <?php echo $material_type ?>'
-            data-post-id='<?php echo get_the_ID(); ?>' download data-file-id="<?php echo $attachment_id?>">
+          <a href='<?php echo esc_url($download_link); ?>' class='btn-buy material-download'
+            data-post-id='<?php echo get_the_ID(); ?>' download>
             <div class="btn-buy__text"><?php echo esc_html($button_text); ?></div>
             <div class="btn-buy__filename"><?php echo esc_html(get_the_title($attachment_id)); ?></div>
-            <div class="remove">XXX</div>
-</div>
+          </a>
           <?php
         } catch (Exception $e) {
           ?>
@@ -37,9 +32,7 @@ if ($args['is_purchased'] || $args['is_free'] || current_user_can('administrator
       endforeach;
     endif;
   endforeach;
-  if($args['metodic_material_once']){
-    return;
-  }
+
   $metodic_file_url = get_post_meta($post_id, 'metodic_file_url', true);
   if ($metodic_file_url): ?>
     <a href="<?php echo esc_url($metodic_file_url) ?>" class="btn-buy material-download">
